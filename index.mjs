e9917660599c08a81f7b91527db870fe1cbaee43
acarolos <cerdacarolos@gmail.com>
2024-03-08 06:09:52 -0800

    Initial commit

---
+++ index.mjs
@@ -0,0 +1,10 @@
+import { feetToMeter } from "unit-convert-universal-js";
+
+// usage
+// const feet = 5.9;
+// const meter = feetToMeter(feet);
+// console.log(meter);
+
+module.exports = {
+    feetToMeter
+}
